eb9fbf4cbf5f5e6771b9481e56df7427fe311817
ChrisFlacko <avanchrisi@gmail.com>
2026-06-12 21:55:50 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+int main(void) {
+    /* Initialize RTOS */
+    
+    /* Scheduler loop */
+    while (1) {
+        /* Run scheduler */
+    }
+    
+    return 0;
+}
--- os.c
+++ os.c
@@ -1,12 +1,3 @@
 /* RTOS Kernel */
 
-int main(void) {
-    /* Initialize RTOS */
-    
-    /* Scheduler loop */
-    while (1) {
-        /* Run scheduler */
-    }
-    
-    return 0;
-}
+
